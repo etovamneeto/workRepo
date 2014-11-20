e34f8f85fa09577650686b21bd818afded756882
unknown <TestUser@lovachevss.typhoon.lan>
2014-11-20 15:42:30 +0300

    Начало положено, разобраться с чтением файла в лист

---
+++ TestProject/TestProject/Main.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+namespace TestProject
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            string path = "C:\\Users\\lovachevss\\Desktop\\workRepo\\";
+            string fileName = "new_file.txt";
+
+            FileStream newFile1 = new FileStream(path+fileName, FileMode.OpenOrCreate, FileAccess.Write);
+            FileStream newFile2 = new FileStream(path + "Full_C00_C80.log", FileMode.Open, FileAccess.Read);
+
+            StreamWriter writer1 = new StreamWriter(newFile1);
+            StreamReader reader2 = new StreamReader(newFile2);
+
+            List<string> list = new List<string>();
+            
+
+
+            writer1.Close();
+            reader2.Close();
+            
+
+
+        }
+
+    }
+}
