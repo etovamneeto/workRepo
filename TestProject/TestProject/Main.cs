5b74c5e786d2b2daababe5b6d31dbe5547c9ee99
etovamneeto <lovachevss@gmail.com>
2014-12-10 16:40:09 +0300

    Сделал вывод значений, привязанный к годам, которые участвуют или не участвуют в построении графика. Надо выделять остальную информацию и писать ее в нужные строки.

--- TestProject/TestProject/Main.cs
+++ TestProject/TestProject/Main.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Text;
 using System.IO;
 using System.Text.RegularExpressions;
+using Excel = Microsoft.Office.Interop.Excel;
 
 namespace TestProject
 {
@@ -67,8 +68,58 @@ namespace TestProject
                 }
             }
 
-            Console.ReadKey();
+            //Работа с выводом в Excel-файл; Всю логику работы по выделению данных пиши выше этого места.
+            Excel.Application excelAppication = new Excel.Application();//открываем приложение Excel
+            excelAppication.Visible = true;//Отображаем окно Excel-приложения
+            excelAppication.DisplayAlerts = true;//Запрос на сохранение при изменении
+            excelAppication.StandardFont = "Times-New-Roman";//Задаем шрифт и его параметры
+            excelAppication.StandardFontSize = 12;
 
+            excelAppication.SheetsInNewWorkbook = 1;//Указываем количество листов в новой рабочей книге
+            excelAppication.Workbooks.Add(Type.Missing);//Добавили рабочую книгу в приложение
+            Excel.Workbook excelAppWorkbook = excelAppication.Workbooks[1];//Получили ссылку на рабочую книгу
+            Excel.Worksheet excelAppWorksheet = (Excel.Worksheet)excelAppWorkbook.Worksheets.get_Item(1);//Получили ссылку на лист рабочей книги
+
+            Excel.Range excelAppCells = excelAppWorksheet.get_Range("A1","J1");//Переменная для работы с ячейками; пока что здесь ячейки под надпись SIR Full (A1-J1)
+            excelAppCells.VerticalAlignment = Excel.Constants.xlCenter;//Выравнивание по центру
+            excelAppCells.HorizontalAlignment = Excel.Constants.xlCenter;
+            excelAppCells.Merge();//Слияние ячеек
+            excelAppCells.Value2 = "SIR Full";//Выводим в объединенные ячейки текст
+            excelAppCells.get_Characters(1, 4).Font.Bold = true;
+
+            //начинаем работу по выводу значений в таблицу; привязка сделана к годам (содержимое years)
+            excelAppCells = excelAppWorksheet.get_Range("A2",Type.Missing);//А2 - вывод заголовка столбца А
+            excelAppCells.Value2 = "year";
+            excelAppCells = excelAppWorksheet.get_Range("B2", Type.Missing);//В2 - вывод заголовка столбца В
+            excelAppCells.Value2 = "RUS";
+
+            for (int i = 0; i < years[0]-1986; i++)//заполнение тех строк и столбцов, которые не участвуют в построении графика (в данном случае строки 3-8)
+            {
+                excelAppCells = (Excel.Range)excelAppWorksheet.Cells[i + 3, 1];//Выводим в столбец А (years)
+                excelAppCells.Value2 = 1986 + i;
+
+                excelAppCells = (Excel.Range)excelAppWorksheet.Cells[i + 3, 2];//Выводим в столбец В (RUS)
+                excelAppCells.Value2 = 1;
+            }
+            for (int i = 0; i < years.Count; i++)//вывод строк, которые участвую в построении графика
+            {
+                excelAppCells = (Excel.Range)excelAppWorksheet.Cells[i + 3 + years[0] - 1986, 1];//выводим в столбец А (years)
+                foreach (int val in years)
+                {
+                    excelAppCells.Value2 = years[i];
+                    excelAppCells.Font.ColorIndex = 5;
+                }
+
+                excelAppCells = (Excel.Range)excelAppWorksheet.Cells[i + 3 + years[0] - 1986, 2];//Выводим в столбец B (RUS)
+                excelAppCells.Value2 = 1;
+                excelAppCells.Font.ColorIndex = 5;      
+            }
+
+            Console.WriteLine(years[years.Count-1]);
+            Console.WriteLine(years[years.Count - 1]-years[0]);
+            Console.ReadKey();
+            excelAppication.Workbooks.Close();
+            excelAppication.Quit();
         }
     }
 }
